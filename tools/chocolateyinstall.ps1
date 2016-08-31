0a37524672b96886cde8efac6f29b35de06efe13
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-08-31 16:46:58 +0900

    Initial commit.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'vmware-workstation-player'
+  fileType       = 'EXE'
+  url64bit       = 'http://www.vmware.com/go/tryplayerpro-win-64'
+  softwareName   = 'vmware-workstation-player*'
+  checksum64     = 'D63E0D2B95E1BA23AFD1A9D04137B0FF8EA58CFC'
+  checksumType64 = 'sha1'
+  silentArgs     = "/s /v/qn EULAS_AGREED=1"
+  validExitCodes = @(0, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$registryKey = '{537B7F85-2B95-44ED-8D90-765F6F36D666}'
+$msiDir = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryKey").InstallSource
+
+$packageArgs = @{
+  packageName    = 'vmware-workstation-player'
+  fileType       = 'MSI'
+  silentArgs     = "`"$msiDir$registryKey.msi`" /quiet"   # MSI file name is here to get around choco's bug.
+  file           = ''                                     # This will be ignored.
+  validExitCodes = @(0, 3010)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
