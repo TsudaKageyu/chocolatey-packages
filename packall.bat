c452274efbd82d49b23468e151d7b382cc5eb614
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 14:56:02 +0900

    Initial commit of the xrecord3 package.

--- packall.bat
+++ packall.bat
@@ -1,4 +1,6 @@
 @echo off
+del *.nupkg
 choco pack .\vmware-workstation-player\vmware-workstation-player.nuspec
+choco pack .\xrecode3\xrecode3.nuspec
 choco pack .\xrecode3.install\xrecode3.install.nuspec
 choco pack .\xrecode3.portable\xrecode3.portable.nuspec
