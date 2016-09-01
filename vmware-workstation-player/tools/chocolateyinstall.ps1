d47b9512ebdc45bc8af4d1e0d210791b62bb7d32
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 10:05:41 +0900

    More secure hash.

--- vmware-workstation-player/tools/chocolateyinstall.ps1
+++ vmware-workstation-player/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   fileType       = 'EXE'
   url64bit       = 'https://download3.vmware.com/software/player/file/VMware-player-12.1.1-3770994.exe'
   softwareName   = 'vmware-workstation-player*'
-  checksum64     = 'D63E0D2B95E1BA23AFD1A9D04137B0FF8EA58CFC'
-  checksumType64 = 'sha1'
+  checksum64     = '1CD93C067785D80D347F4479826F9399DD713573EC04659F74477917E605BFD1'
+  checksumType64 = 'sha256'
   silentArgs     = "/s /v/qn EULAS_AGREED=1"
   validExitCodes = @(0, 3010)
 }
