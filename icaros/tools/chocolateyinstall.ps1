70a9d745deb26285a3ac8689416cef96f73328f1
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 17:49:06 +0900

    Initial commit of the Icaros package. (But private use only)

---
+++ icaros/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'icaros'
+  fileType       = 'EXE'
+  url            = 'http://files2.majorgeeks.com/e0ec4ca047662253f6d21b908c771c1a/multimedia/Icaros_v3.0.0.exe'
+  softwareName   = 'icaros*'
+  checksum       = 'A6C4824197EE4BE85F6D99DD3EDEC163104F30999C96AB4550794833E5523B5A'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
--- packall.bat
+++ packall.bat
@@ -4,3 +4,4 @@ choco pack .\vmware-workstation-player\vmware-workstation-player.nuspec
 choco pack .\xrecode3\xrecode3.nuspec
 choco pack .\xrecode3.install\xrecode3.install.nuspec
 choco pack .\xrecode3.portable\xrecode3.portable.nuspec
+choco pack .\icaros\icaros.nuspec
