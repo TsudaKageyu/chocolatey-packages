c1efd8188900f5de88c33a7f0f8e7a6a7ab2e3fc
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 11:28:56 +0900

    Initial commit of the XRECODE3 package.

---
+++ xrecode3/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'xrecode3'
+  fileType       = 'EXE'
+  url            = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_32bit_1.35.exe'
+  url64bit       = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_64bit_1.35.exe'
+  softwareName   = 'xrecode3*'
+  checksum       = '5DD3AEF92D9606E244350D3007FA161F453B4DB59D1D1025B80533B48A8C38C3'
+  checksumType   = 'sha256'
+  checksum64     = '1345FD7FD396A98429945BA7336A985EA58167E2CF467EBC065EC6DD640E1DAB'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ xrecode3/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$file = (Get-ItemProperty `
+  -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{0870F25F-0A0A-4614-A1AD-7477C248502E}_is1").UninstallString
+
+$packageArgs = @{
+  packageName    = 'xrecode3'
+  fileType       = 'EXE'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  file           = $file
+  validExitCodes = @(0)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
