75e00944a78c5f331a5ea31dda233def1145c310
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 13:13:07 +0900

    xrecode3: Support 32-bit uninstaller.

--- xrecode3/tools/chocolateyuninstall.ps1
+++ xrecode3/tools/chocolateyuninstall.ps1
@@ -1,7 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$file = (Get-ItemProperty `
-  -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{0870F25F-0A0A-4614-A1AD-7477C248502E}_is1").UninstallString
+if (Get-ProcessorBits -eq 64) {
+  $registryKey = '{0870F25F-0A0A-4614-A1AD-7477C248502E}_is1'
+} else {
+  $registryKey = '{9E725299-47B7-4B43-8233-37DD430932E4}_is1'
+}
+$file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryKey").UninstallString
+$file = $file -Replace '"', ''
 
 $packageArgs = @{
   packageName    = 'xrecode3'
