f9be864272bd0209fd6d33eec22f281886afe17f
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-01 14:35:38 +0900

    Add a batch to compile all the package at once.

---
+++ packall.bat
@@ -0,0 +1,4 @@
+@echo off
+choco pack .\vmware-workstation-player\vmware-workstation-player.nuspec
+choco pack .\xrecode3.install\xrecode3.install.nuspec
+choco pack .\xrecode3.portable\xrecode3.portable.nuspec
