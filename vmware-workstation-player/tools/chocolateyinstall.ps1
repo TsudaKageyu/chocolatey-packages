a423a152011b16a11b4be497b1ddddc0a9b3446a
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-09-14 16:53:11 +0900

    VMWare Player: Update to v12.5.0.

--- vmware-workstation-player/tools/chocolateyinstall.ps1
+++ vmware-workstation-player/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'vmware-workstation-player'
   fileType       = 'EXE'
-  url64bit       = 'https://download3.vmware.com/software/player/file/VMware-player-12.1.1-3770994.exe'
+  url64bit       = 'https://download3.vmware.com/software/player/file/VMware-player-12.5.0-4352439.exe'
   softwareName   = 'vmware-workstation-player*'
-  checksum64     = '1CD93C067785D80D347F4479826F9399DD713573EC04659F74477917E605BFD1'
+  checksum64     = 'A88E8E39308EF0C2D0B1DB27B04F1AA40726CBCCBB5AD5120D6DF9FC01609FF5'
   checksumType64 = 'sha256'
   silentArgs     = "/s /v/qn EULAS_AGREED=1"
   validExitCodes = @(0, 3010)
