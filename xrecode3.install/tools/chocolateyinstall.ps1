73b2e1c13da3169708e65105ce33ff009bdff5c2
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-10-12 09:34:12 +0900

    xrecode3: Update to v1.39.

--- xrecode3.install/tools/chocolateyinstall.ps1
+++ xrecode3.install/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = 'xrecode3.install'
   fileType       = 'EXE'
-  url            = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_32bit_1.38.exe'
-  url64bit       = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_64bit_1.38.exe'
+  url            = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_32bit_1.39.exe'
+  url64bit       = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_64bit_1.39.exe'
   softwareName   = 'xrecode3.install*'
-  checksum       = 'E85600F0CDC5D2EFA1A70CAF013CCE4EB107BE4E9E10324084D6FFBE35AB507E'
+  checksum       = '3CF8D5C770BF4A9B44CD9AFD1315E54C160083DAC40F0F674CDD6AED76D75E77'
   checksumType   = 'sha256'
-  checksum64     = 'B151094FFCD73DEE9B19FCAB8DB20F521F88A3EB500982165222FCCB8D8F9A92'
+  checksum64     = '75C5EFE6FFB847BB6369DD925DF25DA2C84843770B93024928227006CB00116A'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- xrecode3.portable/tools/chocolateyinstall.ps1
+++ xrecode3.portable/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 
 $packageArgs = @{
   packageName    = 'xrecode3.portable'
-  url            = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_32bit_1.38_portable.7z'
-  url64bit       = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_64bit_1.38_portable.7z'
+  url            = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_32bit_1.39_portable.7z'
+  url64bit       = 'http://xrecode.com/xrecode3/download/setup_xrecode3_win_64bit_1.39_portable.7z'
   softwareName   = 'xrecode3.portable*'
-  checksum       = '1B460E25B80C5834F18E395AA593E11FC984B50B209420E865C68248F69D0980'
+  checksum       = 'A3010C59CA5C36D0C6903DC3BE80374A8A6D40EC4A028656B1FD04661785FAE8'
   checksumType   = 'sha256'
-  checksum64     = '2B956FC08D83DAEFADAE9A3DD53DFFD6C2486F8C046584678E8FBD340A165F19'
+  checksum64     = '4CD4365B48B76999C9B740CCC1F22AAFE4AA91DF78EE521B3C721307A6F5F652'
   checksumType64 = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
